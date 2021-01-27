8d39f1c818fe69afd1458e3d98a63e55a8edecf9
Tomasz Kowalik <tomaszkowalik85@gmail.com>
2021-01-27 23:46:44 +0100

    sucsessful fetching weather

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ input.addEventListener('keydown', (e) => {
     if (e.keyCode === 13) fetchWeather(e.target.value);
 });
 
-button.addEventListener('click', ()=>{
+button.addEventListener('click', () => {
     fetchWeather(input.value);
 });
 
@@ -13,7 +13,7 @@ async function fetchWeather(city) {
     try {
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=c09e360baf439a72fb2afb607f153694`);
         console.log(response.data);
-        createWidget(response.data)
+        createElements(response.data)
     }
     catch (error) {
         if (error.response) {
@@ -23,14 +23,22 @@ async function fetchWeather(city) {
 };
 
 
-function createElements(){
-    document.querySelector('#weather')
+function createElements({ main, name, weather }) {
+    const {main:conditions, icon} = weather[0]
+    document.querySelector('#weather').innerHTML = `
+    <p id="city" class="card-title">${name}</p>
+    <p id="conditions">${conditions}</p>
+    <div class="card-content" style="padding-top: 0">
+    <div class="col s4"></div>
+    <span class="degree col s4">${Math.round(main.temp)}&#176</span>
+    <img class="col s4" src="http://openweathermap.org/img/wn/${icon}@2x.png">
+    </div>`
+    console.log(conditions, icon)
 }
-function createWidget({main, name}){
-    console.log('temp:',Math.round(main.temp))
-    console.log('temp min:',Math.round(main.temp_min))
-    console.log('temp max:',Math.round(main.temp_max))
-    console.log('feels like:',Math.round(main.feels_like))
+function createWidget({ main, name }) {
+    console.log('temp min:', Math.round(main.temp_min))
+    console.log('temp max:', Math.round(main.temp_max))
+    console.log('feels like:', Math.round(main.feels_like))
     console.log(name)
 
 }

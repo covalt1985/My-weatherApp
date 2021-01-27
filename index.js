9f2b43e32b812d1076d6246e8aa2bdb006b2482a
Tomasz Kowalik <tomaszkowalik85@gmail.com>
2021-01-27 20:36:49 +0100

    add async func, queryselectors and event

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-async function fetchWeather(city){
-const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c09e360baf439a72fb2afb607f153694`)
-console.log((response.data.main.temp - 272).toFixed(1))
-}
+async function fetchWeather(city) {
+    try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=c09e360baf439a72fb2afb607f153694`);
+        console.log(response.data);
+    }
+    catch (error) {
+        if (error.response) {
+            console.log('Error:', error.response.data.message);
+        };
+    };
+};
 
-const input = document.querySelector('input')
+const input = document.querySelector('input');
+const button = document.querySelector('a');
+
+input.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) fetchWeather(e.target.value);
+});
+
+button.addEventListener('click', ()=>{
+    fetchWeather(input.value);
+})
+
+
+
+

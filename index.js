05e93da1b8dca8d1335d4afe0c70aab0d50e625b
Tomasz Kowalik <tomaszkowalik85@gmail.com>
2021-01-29 01:12:03 +0100

    remove button add min max temps

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ function toggleClass() {
 
 //events trigger func that fetches for weather
 input.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {fetchWeather(e.target.value);
-    weatherDiv.classList.add('scale-out')};
+    if (e.keyCode === 13) {
+        fetchWeather(e.target.value);
+        weatherDiv.classList.add('scale-out')
+    };
 });
-button.addEventListener('click', () => {
+/* button.addEventListener('click', () => {
     fetchWeather(input.value);
     weatherDiv.classList.add('scale-out');
-});
+}); */
 
 async function fetchWeather(city) {
     try {
@@ -35,7 +37,7 @@ async function fetchWeather(city) {
 
 //creates html with fetched data
 function createElements({ main, name, weather }) {
-    const container = document.querySelector('#weather')
+    const container = document.querySelector('#weather');
     const { main: conditions, icon } = weather[0];
     container.innerHTML = `
     <p id="city" class="card-title">${name}</p>
@@ -44,21 +46,37 @@ function createElements({ main, name, weather }) {
     <div class="col s4"></div>
     <span class="degree col s4">${Math.round(main.temp)}&#176</span>
     <img class="col s4" src="http://openweathermap.org/img/wn/${icon}@2x.png">
-    </div>`;
-    console.log(conditions, icon);
-    weatherDiv.classList.remove('scale-out')
-}
+    <div class="row">
+    <div class="col s12"><p style="text-align: center; -webkit-text-stroke-width: medium">
+    max:  ${Math.round(main.temp_max)}&#176  &nbsp  min:  ${Math.round(main.temp_min)}&#176</p></div>
+    </div>
+    </div>
+    `
+    weatherDiv.classList.remove('scale-out');
+};
 
+//for future purpose
 function createWidget({ main, name }) {
-    console.log('temp min:', Math.round(main.temp_min))
-    console.log('temp max:', Math.round(main.temp_max))
-    console.log('feels like:', Math.round(main.feels_like))
-    console.log(name)
+    console.log('feels like:', Math.round(main.feels_like));
+    console.log(name);
+};
 
-}
-
-
-
+window.addEventListener('DOMContentLoaded', () => {
+    weatherDiv.classList.add('scale-out');
+    async function getIP() {
+        try {
+            const ip = await axios.get('https://api.ipify.org/')
+            const location = await axios.get(`http://api.ipstack.com/${ip.data}?access_key=2aa0ad6c48283a995770a0dfc8a2602d`)
+            fetchWeather(location.data.country_name);
+        }
+        catch (error) {
+            if (error.response) {
+                console.log('Error:', error.response.data.message);
+            };
+        };
+    };
+    getIP();
+});
 
 
 

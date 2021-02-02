1101f5f5840cf0453328040c2301e08a5d5c2c44
Tomasz Kowalik <tomaszkowalik85@gmail.com>
2021-02-02 23:42:19 +0100

    Add preloader and error message

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
 const input = document.querySelector('input');
 const weatherDiv = document.querySelector('#weather');
+const preloader = document.querySelector('#preloader');
+
 
 //toggles inputs style when focusing
 input.addEventListener('focusin', toggleClass);
 input.addEventListener('focusout', toggleClass);
+
 function toggleClass() {
     input.classList.toggle('city_name');
 };
 
 //triggers function that fetch for weather
 input.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {
+
+    if (e.code === 'Enter') {
+        preloader.classList.remove('preloader');
         fetchWeather(e.target.value);
         weatherDiv.classList.add('scale-out');
     };
 });
 
 async function fetchWeather(city) {
+
     try {
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=c09e360baf439a72fb2afb607f153694`);
         input.value = '';
         console.log(response.data);
         createElements(response.data);
     }
+
     catch (error) {
         if (error.response) {
-            console.log('Error:', error.response.data.message);
+            weatherDiv.classList.remove('scale-out');
+
+            weatherDiv.innerHTML = `
+            <p id="city" class="card-title">${(error.response.data.message).toUpperCase()}</p>
+            <p style="text-align:center; margin-top:2em;">
+            <i class="material-icons prefix" style=" font-size: xxx-large;">cloud_off</i></p>
+            `;
+            preloader.classList.add('preloader');
         };
     };
 };
 
 //creates html with fetched data
 function createElements({ main, name, weather }) {
-    const container = document.querySelector('#weather');
     const { main: conditions, icon } = weather[0];
-    container.innerHTML = `
+
+    weatherDiv.innerHTML = `
     <p id="city" class="card-title">${name}</p>
     <p id="conditions">${conditions}</p>
     <div class="card-content" style="padding-top: 0">
@@ -47,24 +61,32 @@ function createElements({ main, name, weather }) {
     </div>
     </div>
     `;
+
     weatherDiv.classList.remove('scale-out');
+    preloader.classList.add('preloader');
+
 };
 
 //shows clients country weather on startup
 window.addEventListener('DOMContentLoaded', () => {
+
     weatherDiv.classList.add('scale-out');
+
     async function getLocation() {
         try {
             const ip = await axios.get('https://api.ipify.org/')
             const location = await axios.get(`http://api.ipstack.com/${ip.data}?access_key=2aa0ad6c48283a995770a0dfc8a2602d`)
+
             fetchWeather(location.data.country_name);
         }
+
         catch (error) {
             if (error.response) {
                 console.log('Error:', error.response.data.message);
             };
         };
     };
+
     getLocation();
 });
 
